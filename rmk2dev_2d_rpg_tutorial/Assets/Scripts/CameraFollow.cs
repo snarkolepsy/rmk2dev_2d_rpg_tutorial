7200a80ab64719c80a6e167850b72b91b4479171
David Do <david.pham.do@gmail.com>
2016-09-08 17:43:38 -0700

    What I've done so far
    2D sprite animation, player control, scene transitions, importing maps from Tiled to Unity while preserving collision information.

---
+++ rmk2dev_2d_rpg_tutorial/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float m_speed = 0.1f;
+    Camera myCam;
+
+	// Use this for initialization
+	void Start () {
+        myCam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // optional, fixes scaling of pixels
+        myCam.orthographicSize = (Screen.height / 100.0f) / 3.0f; 
+
+        if (target) {
+            // linear interpolation
+            transform.position = Vector3.Lerp(transform.position, target.position, m_speed) + new Vector3(0.0f, 0.0f, -10.0f);
+        }
+	}
+}
---
+++ rmk2dev_2d_rpg_tutorial/Assets/Scripts/Warp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+
+    public Transform warpTarget;
+
+    IEnumerator OnTriggerEnter2D(Collider2D other) {
+        ScreenFader sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader> ();
+
+        yield return StartCoroutine(sf.FadeToBlack()); // Fade to black screen
+
+        Debug.Log("An object collided.");
+        other.gameObject.transform.position = warpTarget.position; // reposition
+        Camera.main.transform.position = warpTarget.position;
+
+        yield return StartCoroutine(sf.FadeToClear()); // Fade to clear
+    }
+}
